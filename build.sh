6d07d0cae7da0e6e0d191208a199f08f11deb3ba
rog-net <76084077+rog-net@users.noreply.github.com>
2020-12-20 01:51:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+echo "* ============== Python3 Cross Wrapper ============== *"
+echo "|                       BETA 0.1                      |"
+echo "* =================================================== *"
+
+mkdir build_out
+mkdir output_bin
+mkdir run_out
+
 nohup docker build ./platforms/Linux/ --target arm64 -t python3_cross_packager/linux-arm64 >build_out/linux-arm64.out 2>&1 &
 nohup docker build ./platforms/Linux/ --target armel -t python3_cross_packager/linux-armel >build_out/linux-armel.out 2>&1 &
 nohup docker build ./platforms/Linux/ --target armhf -t python3_cross_packager/linux-armhf >build_out/linux-armhf.out 2>&1 &
@@ -7,11 +15,13 @@ nohup docker build ./platforms/Linux/ --target mips64le -t python3_cross_package
 nohup docker build ./platforms/Linux/ --target ppc64le -t python3_cross_packager/linux-ppc64le >build_out/linux-ppc64le.out 2>&1 &
 nohup docker build ./platforms/Linux/ --target s390x -t python3_cross_packager/linux-s390x >build_out/linux-s390x.out 2>&1 &
 
+
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] Building"
 while true
 do
 	process0=`ps aux | grep "docker build" | grep -v grep`;
 	if [ "$process0" == "" ]; then
-		echo -e "\nALL Finished!";
+		echo -e "\n[$(date "+%Y-%m-%d %H:%M:%S")]ALL Finished!";
 		break;
 	else
 		echo -e ".\c"
