5a109b096f70ed1f8262d8af05545ae15457b271
rog-net <76084077+rog-net@users.noreply.github.com>
2020-12-22 16:36:44 +0800

    Add files via upload

---
+++ platforms/FreeBSD-amd64/run.sh
@@ -0,0 +1,1 @@
+PROJECT_NAME=$1 SOURCE_CODE=../../$2 vagrant up --provision
---
+++ platforms/FreeBSD-amd64/startup.sh
@@ -0,0 +1,11 @@
+sudo su
+ifconfig
+sed -i '' 's/#ASSUME_ALWAYS_YES = false;/ASSUME_ALWAYS_YES = true;/g' /usr/local/etc/pkg.conf
+pkg install python37 zip
+cp -r /home/vagrant/source_code/ /home/vagrant/workspace
+cd /home/vagrant/workspace
+chmod +x wrapper/FreeBSD.sh
+./wrapper/FreeBSD.sh
+cd dist
+zip $PROJECT_NAME-$(uname)-$(uname -m).zip *
+mv *.zip /home/vagrant/output_bin
